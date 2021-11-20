c564ee4c987ebf30d9195b6c6bbd68538bdf33f9
takurinton <takurinton@takurinton.com>
2021-11-20 10:40:40 +0900

    add: form

---
+++ src/pages/Blog/utils/Form.tsx
@@ -0,0 +1,17 @@
+export const Form = ({
+  id,
+  title,
+  is_open,
+  contents,
+  pub_date,
+}: {
+  id: number;
+  title: string;
+  is_open: boolean;
+  contents: string;
+  pub_date: string;
+}) => {
+  return (
+    <>hoge</>
+  );
+};
