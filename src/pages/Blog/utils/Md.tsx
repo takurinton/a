ecee69998dfd9d1ffdd42f8afdb490b24f83fbf5
takurinton <takurinton@takurinton.com>
2021-11-20 11:44:14 +0900

    add: react markdown

--- src/pages/Blog/utils/Md.tsx
+++ src/pages/Blog/utils/Md.tsx
@@ -1,11 +1,11 @@
-import { Box } from "@chakra-ui/layout";
 import { chakra } from "@chakra-ui/system";
+import ReactMarkdown from "react-markdown";
 
 export const Md = ({ title, text }: { title: string; text: string }) => {
   return (
     <>
       <chakra.h1 fontSize='2rem' h='100px'>{title}</chakra.h1>
-      <chakra.p fontSize='1.2rem'>{text}</chakra.p>
+      <ReactMarkdown>{text}</ReactMarkdown>
     </>
   );
 };
