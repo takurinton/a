343a6d819b1b054a95ca60185d66b0baa572ad49
takurinton <takurinton@takurinton.com>
2021-11-21 00:11:50 +0900

    add: new page

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import { ChakraProvider } from "@chakra-ui/react"
 import Home from './pages/Home';
 import Login from "./pages/Login";
 import { Posts, Post } from './pages/Blog';
+import { New } from './pages/Blog/New';
 import Header from './components/Header';
 import { getToken } from './utils/getToken';
 
@@ -16,7 +17,7 @@ export function App() {
         <Route path='/' component={Home} />
         <Route path='/blog' component={Posts} />
         <Route path='/blog/:id' component={Post} />
-        <Route path='/about' component={Home} />
+        <Route path='/blog/new' component={New} />
         <Route path='/login' component={() => <Login isAdmin={isAdmin} />}/>
       </Router>
     </ChakraProvider>
