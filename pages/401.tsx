6385358d037e007b8eaf3f51660c0d396f4556ee
takurinton <takurinton@takurinton.com>
2021-12-31 09:48:36 +0900

    fix

--- pages/401.tsx
+++ pages/401.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@chakra-ui/button";
 import { Box } from "@chakra-ui/layout";
 import { signIn } from "next-auth/client";
+import { useEffect } from "react";
 
 export default function Unauthrized() {
+  useEffect(() => {
+    signIn();
+  }, []);
+  
   return (
-    <Box>
-      <Button onClick={() => signIn()}>ログインし直す</Button>
-    </Box>
+    <></>
   );
 }
