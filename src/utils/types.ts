cc6c978ee0937fcf93284e45f912765d59759002
Takumi Katayama <takurinton@takurinton.com>
2022-01-01 10:36:49 +0900

    Update src/utils/types.ts

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,7 +3,7 @@ export type Post = {
   title:    string;
   contents: string; // これは消す予定
   is_open:  boolean | string;
-  pub_date: Date; // 本当は datetime
+  pub_date: Date;
 };
 
 export type Posts = Post[];
