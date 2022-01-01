69a19b9ffd5a25b5a9f64951d9f5fe944c5d362f
takurinton <takurinton@takurinton.com>
2022-01-01 10:04:57 +0900

    add: types

---
+++ src/utils/types.ts
@@ -0,0 +1,16 @@
+export type Post = {
+  id:       number;
+  title:    string;
+  contents: string; // これは消す予定
+  is_open:     boolean;
+  pub_date: string; // 本当は datetime
+};
+
+export type Posts = Post[];
+
+export type Categories = {
+  category: {
+    id:   number; 
+    name: string;
+  }[];
+};
