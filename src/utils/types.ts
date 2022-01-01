ee6d6eacb5d6c27e8229e5ff61e7e4b20eb39ee7
takurinton <takurinton@takurinton.com>
2022-01-01 10:05:30 +0900

    fix

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,7 +2,7 @@ export type Post = {
   id:       number;
   title:    string;
   contents: string; // これは消す予定
-  is_open:     boolean;
+  is_open:  boolean;
   pub_date: string; // 本当は datetime
 };
 
